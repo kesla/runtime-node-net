67ef346d081cb970f75475fd14bfff01785d4be5
David Björklund <david.bjorklund@gmail.com>
2015-06-09 02:20:09 +0200

    kill qemu child in runner.js

--- test/runner.js
+++ test/runner.js
@@ -2,13 +2,16 @@
 
 const cp = require('child_process');
 const tsml = require('tsml');
-const cmd = `runtimeify ${__dirname}/test.js -o ${__dirname}/initrd &&
-  runtime-qemu ${__dirname}/initrd`;
-const child = cp.exec(cmd);
+const runtimeify = cp.spawn('runtimeify', `${__dirname}/test.js -o ${__dirname}/initrd`.split(' '));
 const finished = require('tap-finished');
 
-child.stdout.pipe(finished(function (results) {
-  process.exit(results.ok ? 0 : 1);
-}));
+runtimeify.once('close', function () {
+  const qemu = cp.spawn('runtime-qemu', [`${__dirname}/initrd`]);
 
-child.stdout.pipe(process.stdout);
+  qemu.stdout.pipe(finished(function (results) {
+    qemu.kill('SIGINT');
+    process.exit(results.ok ? 0 : 1);
+  }));
+
+  qemu.stdout.pipe(process.stdout);
+})
