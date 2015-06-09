cdbd4b006771b26185b19fdc0e614524e52e5354
David Björklund <david.bjorklund@gmail.com>
2015-06-09 19:29:58 +0200

    add a simple http-test

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 const connect = require('./lib/connect');
 const Server = require('./lib/server');
+const Socket = require('./lib/socket');
 
+module.exports.Server = Server;
 module.exports.createServer = function (cb) {
   return new Server(cb);
 }
 
+module.exports.Socket = Socket;
 module.exports.createConnection = connect;
 
 module.exports.connect = connect;
--- test/runner.js
+++ test/runner.js
@@ -3,12 +3,23 @@
 process.exitCode = 1;
 
 const cp = require('child_process');
-const tsml = require('tsml');
-const runtimeify = cp.spawn('runtimeify', `${__dirname}/test.js -o ${__dirname}/initrd`.split(' '));
 const finished = require('tap-finished');
 
-runtimeify.once('close', function () {
-  const qemu = cp.spawn('runtime-qemu', [`${__dirname}/initrd`]);
+const runtimeify = require('runtimeify');
+
+runtimeify.builtins.http = require.resolve('http-node');
+runtimeify.builtins.net = require.resolve('../index');
+
+runtimeify({
+  file: [ __dirname + '/test.js', __dirname + '/http-test.js'],
+  output :__dirname + '/initrd',
+  debug: true
+}, function (err) {
+  if (err) {
+    throw err;
+  }
+
+  const qemu = cp.spawn('runtime-qemu', [ `${__dirname}/initrd`, '--verbose', '--nographic' ]);
 
   qemu.stdout.pipe(finished(function (results) {
     qemu.kill('SIGINT');
@@ -16,4 +27,4 @@ runtimeify.once('close', function () {
   }));
 
   qemu.stdout.pipe(process.stdout);
-})
+});
