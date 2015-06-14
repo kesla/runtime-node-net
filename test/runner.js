c1e78a884a9df518550d0a515ffd640bc669109e
David Björklund <david.bjorklund@gmail.com>
2015-06-14 18:36:17 +0200

    reorder tests

--- test/runner.js
+++ test/runner.js
@@ -4,14 +4,24 @@ process.exitCode = 1;
 
 const cp = require('child_process');
 const finished = require('tap-finished');
-
 const runtimeify = require('runtimeify');
+const split = require('split');
 
 runtimeify.builtins.http = require.resolve('http-node');
 runtimeify.builtins.net = require.resolve('../index');
 
+const input = process.argv.slice(2);
+const files = input.length ?
+  input.map(function (name) {
+    return __dirname + '/' + name + '-test.js'
+  }) : [
+    __dirname + '/server-test.js',
+    __dirname + '/connection-test.js',
+    __dirname + '/http-test.js'
+  ];
+
 runtimeify({
-  file: [ __dirname + '/test.js', __dirname + '/http-test.js'],
+  file: files,
   output :__dirname + '/initrd',
   debug: true
 }, function (err) {
@@ -19,12 +29,20 @@ runtimeify({
     throw err;
   }
 
-  const qemu = cp.spawn('runtime-qemu', [ `${__dirname}/initrd`, '--verbose', '--nographic' ]);
+  const qemu = cp.spawn('runtime-qemu',
+    [ `${__dirname}/initrd`, '--verbose', '--nographic' ]
+  );
 
   qemu.stdout.pipe(finished(function (results) {
+    process.exitCode = results.ok ? 0 : 1;
     qemu.kill('SIGINT');
-    process.exit(results.ok ? 0 : 1);
   }));
+
+  qemu.stdout.pipe(split()).on('data', function (chunk) {
+    if (chunk.toString().indexOf('terminate thread') !== -1) {
+      qemu.kill('SIGINT');
+    }
+  });
 
   qemu.stdout.pipe(process.stdout);
 });
--- test/server-test.js
+++ test/server-test.js
@@ -3,7 +3,6 @@
 const test = require('tape');
 const net = require('net');
 
-/*
 test('createServer listen to port', function (t) {
   const server = net.createServer();
 
@@ -81,32 +80,6 @@ test('createServer() listen, close, listen and close', function (t) {
   async++;
 });
 
-test('createConnection && connect', function (t) {
-  t.equal(net.createConnection, net.connect, 'createConnection & connect are the same');
-  t.end();
-});
-
-test('createServer() & createConnection()', function (t) {
-  const server = net.createServer();
-
-  server.once('connection', function (serverSocket) {
-    serverSocket.once('data', function (chunk) {
-      t.equal(chunk.toString(), 'beep');
-    });
-    serverSocket.write('boop');
-    serverSocket.end();
-  });
-
-  server.listen(0, function () {
-    const clientSocket = net.createConnection(server.address().port);
-    clientSocket.write('beep');
-    clientSocket.once('data', function (chunk) {
-      t.equal(chunk.toString(), 'boop');
-      server.close(t.end.bind(t));
-    });
-  });
-});
-
 test('createServer() with callback', function (t) {
   const server = net.createServer(function (socket) {
     socket.once('data', function (chunk) {
@@ -121,77 +94,4 @@ test('createServer() with callback', function (t) {
     socket.write('beep');
     socket.end();
   });
-});
-
-test('createConnection() local with ip and callback', function (t) {
-  const server = net.createServer(function (socket) {
-    socket.end();
-    server.close(t.end.bind(t));
-  });
-
-  server.listen(0, function () {
-    const socket = net.createConnection(server.address().port, '127.0.0.1', function () {
-      socket.end();
-    });
-  });
-})
-
-test('createConnection() local with object', function (t) {
-  const server = net.createServer(function (socket) {
-    socket.end();
-    server.close(t.end.bind(t));
-  });
-
-  server.listen(0, function () {
-    const socket = net.createConnection({
-      port: server.address().port,
-      host: '127.0.0.1'
-    });
-
-    socket.once('connect', function () {
-      socket.end();
-    });
-  });
-})
-
-test('createConnection() local with object and callback', function (t) {
-  const server = net.createServer(function (socket) {
-    socket.end();
-    server.close(t.end.bind(t));
-  });
-
-  server.listen(0, function () {
-    const socket = net.createConnection({
-      port: server.address().port,
-      host: '127.0.0.1'
-    }, function () {
-      socket.end();
-    });
-  });
-});
-
-test('createConnection() localhost as host', function (t) {
-  const server = net.createServer(function (socket) {
-    socket.end();
-    server.close(t.end.bind(t));
-  });
-
-  server.listen(0, function () {
-    const socket = net.createConnection({
-      port: server.address().port,
-      host: 'localhost'
-    }, function () {
-      socket.end();
-    });
-  });
-});
-*/
-test('createConnection() with external host', function (t) {
-  let async = false;
-  const socket = net.createConnection(80, 'example.com', function () {
-    t.ok(async, 'should be async');
-    t.end();
-    socket.end();
-  });
-  async = true;
 });
